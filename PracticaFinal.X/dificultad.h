799f821a0080648e5352cdfe104860bda0480de7
MartinaOD <202302674@alu.comillas.edu>
2026-04-21 00:30:13 +0200

    Refactor dificultad.h to remove commented code

--- PracticaFinal.X/dificultad.h
+++ PracticaFinal.X/dificultad.h
@@ -2,18 +2,8 @@
 #define DIFICULTAD_H
 
 #include <stdint.h>
-/*
-typedef struct {
-    uint16_t led_on_ticks;
-    uint16_t led_off_ticks;
-    uint16_t input_feedback_ticks;
-} game_speed_t;
 
-// Variable global de velocidad
-extern game_speed_t game_speed;
-*/
-
-/* Configura la dificultad a partir de un carácter recibido por UART */
+// Configura la dificultad a partir de un carácter recibido por UART
 void dificultad_configurar(char nivel);
 
 uint16_t dificultad_get_led_on_ticks(void);
